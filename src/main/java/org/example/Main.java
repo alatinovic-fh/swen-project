5c51dd205315a116e416c70d9e247d861e14340f
Aleksandar Latinovic <alatinovic@student.tgm.ac.at>
2024-11-11 23:30:31 +0100

    removed Test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,5 @@ public class Main {
     public static void main(String[] args) {
         //Start Server
         //Stop Server
-        HttpRequestParser parser = new HttpRequestParser();
     }
 }
